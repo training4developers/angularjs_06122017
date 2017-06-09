2306c03f5277d03265fe0db1da3163ebeb0e2edd
Eric W. Greene <eric@ericwgreene.com>
2017-06-08 17:40:01 -0700

    tweaks demo unit test

--- src/js/app/app.component.spec.js
+++ src/js/app/app.component.spec.js
@@ -3,30 +3,57 @@ import 'angular-mocks';
 
 import { appModule } from './app.module';
 
-describe('app component tests', () => {
+describe('app component tests', function() {
 
-    let mockScope = null;
-    let compileSvc = null;
+    var mockScope = null;
+    var compileSvc = null;
+    var httpBackendSvc = null;
+
+    function getColorData() {
+        return [
+            { id: 1, name: 'blue' },
+            { id: 2, name: 'yellow' }
+        ];
+    }
+
+    function getCarData() {
+        return [
+            { id: 1, make: 'Chevrolet', model: 'Volt', year: 2011, color: 'blue', price: 45000 },
+            { id: 2, make: 'Chevrolet', model: 'Malibu', year: 2011, color: 'blue', price: 33000 }
+        ];
+    }
 
     beforeEach(angular.mock.module(appModule.name));
 
     beforeEach(angular.mock.inject([
-        '$rootScope', '$compile',
-        ($rootScope, $compile) => {
+        '$rootScope', '$compile', '$httpBackend',
+        function($rootScope, $compile, $httpBackend) {
 
             mockScope = $rootScope.$new(true);
             compileSvc = $compile;
+            httpBackendSvc = $httpBackend;
         },
     ]));
 
-    it('dom structure', () => {
+    it('dom structure', function() {
 
-        const linkingFn = compileSvc('<main></main>');
-        const componentElement = linkingFn(mockScope);
+        var linkingFn = compileSvc('<main></main>');
+        var componentElement = linkingFn(mockScope);
+
+        httpBackendSvc.expectGET('http://localhost:3010/colors').respond(getColorData());
+        httpBackendSvc.expectGET('http://localhost:3010/cars').respond(getCarData());
 
         mockScope.$digest();
+        httpBackendSvc.flush();
 
-        expect(componentElement.find('h1').text(), 'Hello World!');
+        expect(componentElement.find('color-tool').find('h1').text()).toBe('Color Tool');
+        expect(componentElement.find('car-tool').find('h1').text()).toBe('Car Tool');
+    });
+
+    afterEach(function() {
+
+        httpBackendSvc.verifyNoOutstandingRequest();
+        httpBackendSvc.verifyNoOutstandingExpectation();
     });
 
 });
